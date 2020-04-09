c2609926486a01a94753f7a6b60f559f34807d69
lununglee <leo.lunung.lee@gmail.com>
2020-04-09 12:12:47 -0700

    spawn obstacles at interval and destroy objects off screen

--- Assets/Scripts/SpawnHandler.cs
+++ Assets/Scripts/SpawnHandler.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class SpawnHandler : MonoBehaviour
 {
+	[SerializeField] Transform	Parent;
 	[SerializeField] GameObject	ObstaclePrefab;
+	[SerializeField] float		StartDelay = 2.0f;
+	[SerializeField] float		Interval = 2.0f;
 	Vector3 SpawnPos = new Vector3(25, 0, 0);
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		Instantiate(ObstaclePrefab, SpawnPos, ObstaclePrefab.transform.rotation);
+		InvokeRepeating("SpawnObstacle", StartDelay, Interval);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		
+	}
+
+	void	SpawnObstacle()
+	{
+		GameObject ObstacleInstance = Instantiate(ObstaclePrefab, SpawnPos, ObstaclePrefab.transform.rotation);
+		ObstacleInstance.transform.parent = Parent;
 	}
 }
