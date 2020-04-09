c2f8d4615ec20a016de882655173ede69ae64608
lununglee <leo.lunung.lee@gmail.com>
2020-04-09 11:20:55 -0700

    adde player and basic player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	/** Game Config */
+	Rigidbody	Player;
+	[SerializeField] float	JumpForce = 500.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Player = GetComponent<Rigidbody>();
+		float	JumpForceThisFrame = JumpForce * Time.deltaTime;
+		Player.AddRelativeForce(Vector3.up * JumpForceThisFrame);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
