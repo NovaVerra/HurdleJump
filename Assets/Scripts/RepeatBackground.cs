a71cd16191d434ccffc9d7c28f17fee3f894d675
lununglee <leo.lunung.lee@gmail.com>
2020-04-09 12:31:29 -0700

    repeat background

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+	[SerializeField] Vector3	StartingPos = new Vector3(47.0f, 9.5f, 4.0f);
+	float	MidwayPoint;
+	// Start is called before the first frame update
+	void Start()
+	{
+		MidwayPoint = GetComponent<BoxCollider>().size.x / 2;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.x < StartingPos.x - MidwayPoint)
+		{
+			transform.position = StartingPos;
+		}
+	}
+}
